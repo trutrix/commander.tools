f4307a545f69b2161c7796a7f9af3da34135264f
Colton Chaffos <trutrix@gmail.com>
2025-06-01 08:30:26 -0600

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/ui'
+  ]
+})
