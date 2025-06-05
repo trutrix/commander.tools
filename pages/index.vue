44da1f9926b21b67c607ef031b9b04c7442ebc05
tRuTrIx <trutrix@gmail.com>
2025-06-04 21:26:18 -0600

    Updated button styles

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <div class="flex flex-col items-center justify-center h-screen">
     <h1 class="text-3xl font-bold mb-4">Commander Tools</h1>
     <NuxtLink to="/luck-bobblehead" class="mb-4" >
-      <UButton color="primary" label="Luck Bobblehead" />
+      <UButton class="w-80 md:w-50 h-12" color="primary" label="Luck Bobblehead" />
     </NuxtLink>
     <NuxtLink to="/coin-flipper">
-      <UButton color="secondary" label="Coin Flipper" />
+      <UButton class="w-80 md:w-50 h-12" color="secondary" label="Coin Flipper"/>
     </NuxtLink>
   </div>
 </template>
